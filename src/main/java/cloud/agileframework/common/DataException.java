26b171c65594416859adef12e2c207ea3501e1e0
mydeathtrial <mydeathtrial@163.com>
2022-09-14 19:01:15 +0800

    build(统一升版): 统一升版

---
+++ src/main/java/cloud/agileframework/common/DataException.java
@@ -0,0 +1,26 @@
+package cloud.agileframework.common;
+
+/**
+ * 持久层异常
+ */
+public class DataException extends RuntimeException{
+    public DataException() {
+        super();
+    }
+
+    public DataException(String message) {
+        super(message);
+    }
+
+    public DataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DataException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
